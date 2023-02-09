e8e56fdaf7fb0673d7d89833418cbe28902e6ea5
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2023-02-08 21:43:36 -0300

    Arrumando doutorado e adicionando contribuições

--- grace.py
+++ grace.py
@@ -4,12 +4,12 @@ def resumo():
 
 
 def doutorado():
-    mensagem = "Formou-se em 1944 como a primeira da turma e foi designada para a Bureau of Ships Computation Project da Harvard University como tenente júnior. Ela serviu na equipe de programação Mark I computer dirigida por Howard H. Aiken."
+    mensagem = "obteve um doutorado em Matemática por Yale em 1934 e, quando os EUA entraram na Segunda Guerra Mundial, abandonou seu trabalho de professora de matemática e ingressou na Marinha, onde chegou à patente de contra-almirante."
     return mensagem
 
 
 def contribuicoes():
-    mensagem = ""
+    mensagem = "Grace Hopper foi a criadora da COBOL, a primeira linguagem de programação complexa. Formou-se em 1944 como a primeira da turma e foi designada para a Bureau of Ships Computation Project da Harvard University como tenente júnior. Ela serviu na equipe de programação Mark I computer dirigida por Howard H. Aiken."
     return mensagem
 
 
