3cc5b2d9f008ea76477d7e7491caa7c6a72a6bdf
ninicsg <nicolenic48@gmail.com>
2023-02-15 20:45:07 -0300

    resolução problemas

--- aula.py
+++ aula.py
@@ -1,49 +1,27 @@
 def resumo():
-<<<<<<< HEAD
     mensagem = "Geoffrey Everest Hinton CC FRS FRSC (born 6 December 1947)is a British-Canadian cognitive psychologist and computer scientist, most noted for his work on artificial neural networks. "
-=======
-    mensagem = "Geoffrey Everest Hinton CC FRS FRSC (born 6 December 1947)is a British-Canadian cognitive psychologist and computer scientist, most noted for his work on artificial neural networks. "
->>>>>>> 97484b1 (aula info basica)
     return mensagem
 
 
 def doutorado():
-<<<<<<< HEAD
     mensagem = "Hinton foi eleito Fellow da Royal Society (Londres, Reino Unido) em 1998, além de ser o primeiro ganhador do prêmio Rumelhart em 2001. Neste mesmo ano recebeu um doutorado honorário (honoris causa) da Universidade de Edimburgo, além de uma longa lista de prêmios e reconhecimentos nos anos seguintes, como IJCAI Award for Research Excellence (2005), Herzberg Canada Gold Medal for Science and Engineering (2011) e mais um doutorado honoris causa da Université de Sherbrooke (2013)."
-=======
-    mensagem = "Hinton foi eleito Fellow da Royal Society (Londres, Reino Unido) em 1998, além de ser o primeiro ganhador do prêmio Rumelhart em 2001. Neste mesmo ano recebeu um doutorado honorário (honoris causa) da Universidade de Edimburgo, além de uma longa lista de prêmios e reconhecimentos nos anos seguintes, como IJCAI Award for Research Excellence (2005), Herzberg Canada Gold Medal for Science and Engineering (2011) e mais um doutorado honoris causa da Université de Sherbrooke (2013)."
->>>>>>> 97484b1 (aula info basica)
     return mensagem
 
 
 def contribuicoes():
-<<<<<<< HEAD
-    mensagem = ""
-=======
     mensagem = "O pioneiro em Inteligência Artificial"
->>>>>>> 97484b1 (aula info basica)
     return mensagem
 
 
 def artigos():
-<<<<<<< HEAD
     mensagem = "1º artigo mais citado - Imagenet classification with deep convolutional neural networks - 127230 citações" 
-"2º artigo mais citado - Deep learning - 60553 citações"
-"3º artigo mais citado - Dropout: a simple way to prevent neural networks from overfitting - 40878 citações"
-=======
-    mensagem = "1º artigo mais citado - Imagenet classification with deep convolutional neural networks - 127230 citações" 
-"2º artigo mais citado - Deep learning - 60553 citações"
-"3º artigo mais citado - Dropout: a simple way to prevent neural networks from overfitting - 40878 citações"
->>>>>>> 97484b1 (aula info basica)
+    "2º artigo mais citado - Deep learning - 60553 citações"
+    "3º artigo mais citado - Dropout: a simple way to prevent neural networks from overfitting - 40878 citações"
     return mensagem
 
 
 def citacoes():
-<<<<<<< HEAD
     mensagem = "Total de citações em 08/02/2023 as 21:05h 657011"
-=======
-    mensagem = "Total de citações em 08/02/2023 as 21:05h 657011"
->>>>>>> 97484b1 (aula info basica)
     return mensagem
 
 
@@ -57,11 +35,7 @@ def erro():
     return mensagem
 
 
-<<<<<<< HEAD
 print("\nBoa noite! Você está aprendendo sobre Geoffrey Everest Hinton.\n")
-=======
-print("\nBoa noite! Você está aprendendo sobre Geoffrey Everest Hinton.\n")
->>>>>>> 97484b1 (aula info basica)
 
 continuar = True
 while continuar == True:
@@ -107,8 +81,4 @@ while continuar == True:
     else:
         mensagem = erro()
 
-<<<<<<< HEAD
     print(mensagem)
-=======
-    print(mensagem)
->>>>>>> 97484b1 (aula info basica)
