14ec45e105dd8feac1916b56ecf24ab35351a5f0
felipemtoat <felipesoldatellimotta@gmail.com>
2023-06-30 14:18:44 -0300

    Resumo, contribuições e artigos - Grace Hopper

---
+++ ghopper.py
@@ -0,0 +1,82 @@
+def resumo():
+    mensagem = "Grace Brewster Murray Hopper, nascida em 1906, em Nova York, foi uma grande contribuinte no desenvolvimento das linguagens de programação no que são hoje. Atuou na Marinha Americana dos Estados Unidos da América e teve grande influência na indústria computacional."
+    return mensagem
+
+
+def doutorado():
+    mensagem = ""
+    return mensagem
+
+
+def contribuicoes():
+    mensagem = "A cientista contribuiu para a indústria em diversos aspectos. Alguns de seus trabalhos mais marcantes incluem a criação e desenvolvimento da linguagem COBOL - linguagem que foi essencial à simplificação da programação -, além do compilador, ferramenta que, figurativamente falando, traduz o código do programa para uma linguagem legível à maquina."
+    return mensagem
+
+
+def artigos():
+    mensagem = "BEYER, Kurt W. Grace Hopper and the invention of the information age. Mit Press, 2012.\n\nWILLIAMS, Kathleen Broome. Grace Hopper: Admiral of the cyber sea. Naval Institute Press, 2013.\n\nSAMMET, Jean. Farewell to Grace Hopper—end of an era!. Communications of the ACM, v. 35, n. 4, p. 128-131, 1992."
+    return mensagem
+
+
+def citacoes():
+    mensagem = ""
+    return mensagem
+
+
+def sair():
+    mensagem = "\nObrigado pela leitura!"
+    return mensagem
+
+
+def erro():
+    mensagem = "\nOpção inválida!"
+    return mensagem
+
+
+print("\nBoa noite! Você está aprendendo sobre Grace Brewster Murray Hopper.\n")
+
+continuar = True
+while continuar == True:
+
+    opcao = int(
+        input(
+"""
+\nDigite o número correspondente ao menu que você deseja acessar:
+1 - Resumo
+2 - Doutorado
+3 - Contribuições
+4 - Principais Artigos
+5 - Citações
+6 - Sair\n
+"""
+        )
+    )
+
+    if opcao == 1:
+        print("1 - Resumo")
+        mensagem = resumo()
+
+    elif opcao == 2:
+        print("2 - Doutorado")
+        mensagem = doutorado()
+
+    elif opcao == 3:
+        print("3 - Contribuições")
+        mensagem = contribuicoes()
+
+    elif opcao == 4:
+        print("4 - Principais Artigos")
+        mensagem = artigos()
+
+    elif opcao == 5:
+        print("5 - Citações")
+        mensagem = citacoes()
+
+    elif opcao == 6:
+        mensagem = sair()
+        continuar = False
+
+    else:
+        mensagem = erro()
+
+    print(mensagem)
