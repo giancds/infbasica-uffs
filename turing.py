09e9e361a365a1815aa54e30dec4baddc376265f
mariasolek <root@notemariazinha>
2023-06-30 03:35:55 -0300

    Updated turing.py

--- turing.py
+++ turing.py
@@ -1,15 +1,15 @@
 def resumo():
-    mensagem = "Alan Mathison Turing  foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
+    mensagem = "Alan Mathison Turing foi um ateu e homossexual, matemático e criptógrafo britânico, pioneiro da computação e considerado o pai da ciência computacional, uma vez que por meio de suas ideias, foi possível desenvolver o que chamamos hoje de computadores, além de também ser pineiro da inteligência artificial."
     return mensagem
 
 
 def doutorado():
-    mensagem = ""
+    mensagem = "Em 1938, Turing obteve seu PhD pelo Departamento de Matemática da Universidade de Princeton. Durante a Segunda Guerra Mundial, trabalhou para a Sede de Comunicações do Governo, localizada no Centro de decodificação Britânico, em Bletchley Park, no desenvolvimento da Ultra."
     return mensagem
 
 
 def contribuicoes():
-    mensagem = ""
+    mensagem = "Máquina Universal de Turing\nDesenvolveu o Automatic Computing Engine (ACE)\nConstruiu o primeiro computador digital com programa armazenado eletronicamente.\nSistema de programação usado no Ferranti Mark I, o primeiro computador digital eletrônico comercial de 1951.\nTeste de Turing\nDispositivo eletromecânico denominado “The Bombe”"
     return mensagem
 
 
