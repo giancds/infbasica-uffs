41d31cae63f711e1c3baf14d6c5b7de3c40ba180
Luanllp <luan.llp2005@gmail.com>
2024-05-28 14:21:10 -0300

    Minhas alterações referente a segunda parte do arquivo

--- turing.py
+++ turing.py
@@ -4,7 +4,7 @@ def resumo():
 
 
 def doutorado():
-    mensagem = ""
+    mensagem = "John McCarthy obteve um doutorado em matemática pela Universidade de Princeton em 1951. Sua tese foi intitulada Aplicações de Funções Operacionais de Análise Espacial"
     return mensagem
 
 
@@ -14,12 +14,12 @@ def contribuicoes():
 
 
 def artigos():
-    mensagem = ""
+    mensagem = "John McCarthy publicou uma série de artigos notáveis ao longo de sua carreira, muitos dos quais tiveram um impacto significativo no desenvolvimento da inteligência artificial e da ciência da computação. Alguns dos seus principais artigos incluem: \n Programs with Common Sense (1959) - Neste artigo, McCarthy discute a necessidade de dotar os computadores com senso comum, uma ideia fundamental para a inteligência artificial. \n Towards a Mathematical Science of Computation (1961) - Este artigo aborda questões fundamentais relacionadas à teoria da computação e à formalização dos processos computacionais. \n A Basis for a Mathematical Theory of Computation (1963) - Aqui, McCarthy apresenta a linguagem de programação Lisp, que se tornou fundamental no desenvolvimento de inteligência artificial e é amplamente utilizada até hoje. \n Recursive Functions of Symbolic Expressions and Their Computation by Machine, Part I (1960) - Este artigo, escrito juntamente com o cientista da computação Stephen Cole Kleene, introduz a notação de funções recursivas e é fundamental para a compreensão da teoria da computação. \n Artificial Intelligence: A General Survey (1973) - McCarthy fornece uma visão geral abrangente do campo emergente da inteligência artificial, discutindo várias abordagens e aplicações."
     return mensagem
 
 
 def citacoes():
-    mensagem = ""
+    mensagem = "Como fazer um programa de computador compreender coisas como 'eu quero uma xícara de café' é muito difícil mesmo para o mais avançado dos programas de computador. Mas, 'por favor, me dê uma página do livro tal e me mostre onde está a palavra tal', é bem possível."
     return mensagem
 
 
