df7a7aa8de061ce13c1403a3515904b51f65cfc8
João Pedro Xavier <joaopexavier@gmail.com>
2024-05-28 14:21:20 -0300

    Minhas alterações referente a primeira parte do arquivo

--- turing.py
+++ turing.py
@@ -1,5 +1,5 @@
 def resumo():
-    mensagem = "Alan Mathison Turing  foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
+    mensagem = "John McCarthy foi um cientista da computação de notabilidade incontestável, responsável por introduzir e desenvolver o conceito de inteligência artificial. A sua contribuição para essa área começou com a organização de uma conferência emblemática em 1955, onde o termo “inteligência artificial” foi utilizado pela primeira vez, estabelecendo um precedente para décadas de pesquisa subsequente."
     return mensagem
 
 
@@ -9,7 +9,8 @@ def doutorado():
 
 
 def contribuicoes():
-    mensagem = ""
+    mensagem = "Inteligência Artificial (IA):Fundador da IA: McCarthy é amplamente reconhecido como um dos fundadores do campo da Inteligência Artificial.\n Ele cunhou o termo Inteligência Artificial em 1956. Dartmouth Conference: Organizou a conferência seminal em Dartmouth College em 1956, que é considerada o evento fundador da IA como um campo de pesquisa. Linguagens de Programação:\nLISP: McCarthy desenvolveu a linguagem de programação LISP em 1958. LISP tornou-se uma das principais linguagens de programação usadas na pesquisa de IA devido à sua capacidade de manipular símbolos e listas de maneira eficiente.\nComputação em Tempo Compartilhado:\nMcCarthy foi pioneiro na ideia de computação em tempo compartilhado, que permitiu a vários usuários acessar um computador simultaneamente. Esta inovação foi crucial para o desenvolvimento de sistemas operacionais modernos.\nTeoria da Computação e Lógica Formal:\nTrabalhou extensivamente em lógica formal e seus usos em computação. Contribuiu para a teoria da computabilidade e formalização de problemas de IA em termos matemáticos precisos.\nContribuições Acadêmicas:\nInstituições: Lecionou no Instituto de Tecnologia de Massachusetts (MIT), na Universidade de Stanford e na Universidade de Princeton.\nPublicações: McCarthy publicou inúmeros artigos influentes sobre IA, lógica matemática e teoria da computação"
+    
     return mensagem
 
 
