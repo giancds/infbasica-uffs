1c3ab8ec805b9288b9bcca22a653bd460333350c
Jonatan <jonatanmesnerovicz@uffs>
2024-05-28 16:31:26 -0300

    Adicionado artigos e citações

--- turing.py
+++ turing.py
@@ -14,12 +14,22 @@ def contribuicoes():
 
 
 def artigos():
-    mensagem = ""
+    mensagem = ("1947. The Mathematician, The Works of the Mind. ed. por R. B. Heywood, University of Chicago Press, 180–196. \n"
+    + "1951. The Future of High-Speed Computing, Resumo de um discurso no Seminário IBM sobre Computação Científica, novembro de 1949, Proc. Comp. Sem. , IBM, 13. \n"
+    + "1954. The Role of Mathematics in the Sciences and in Society. Address at 4th Conference of Association of Princeton Graduate Alumni, Junho, 16–29. \n"
+    + "1954. The NORC and Problems in High Speed Computing, Address por ocasião da primeira exibição pública da IBM Naval Ordnance Research Calculator, Dez. 2. \n"
+    + "1955. Method in the Physical Sciences, The Unity of Knowledge, ed. por L. Leary, Doubleday, 157–164. \n"
+    + "1955. Can We Survive Technology?, Junho. \n"
+    + "1955. Impact of Atomic Energy on the Physical and Chemical Sciences, Speech - M.I.T. Alumni Day Symposium, Junho 13, Summary, Tech. Rev. 15–17. \n"
+    + "1955. Defense in Atomic War, Paper delivered at a symposium in honor of Dr. R. H. Kent, Dez. 7, 1955, The Scientific Bases of Weapons, Journ. Am. Ordnance Assoc., 21–23. \n"
+    + "1956. The Impact of Recent Developments in Science on the Economy and on Economics, Partial text of a talk at the National Planning Assoc., Washington, D.C., Dez. 12, 1955, Looking Ahead, 4:11. \n")
     return mensagem
 
 
 def citacoes():
-    mensagem = ""
+    mensagem = ("The legend of John von Neumann, Halmos, P.R. (Amer. Math. Monthly 80, 1973). \n"
+    + "John von Neumann and Norbert Wiener: From mathematics to the tecnologies of life and death, Heims, S. J. (Cambridge, MA, MIT Press, 1980). \n"
+    + "John von Neumann, Ulam (Bull. Amer. Math. Soc. 64, pg. 1-49, 1958)")
     return mensagem
 
 
