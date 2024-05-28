53e0c6bb8c2bcc200492e68c6797ffae2448c209
wemely <wemely@uffs>
2024-05-28 17:39:45 -0300

    Alterado as citações , artigos e contribuiçãoes

--- turing.py
+++ turing.py
@@ -14,12 +14,12 @@ def contribuicoes():
 
 
 def artigos():
-    mensagem = ""
+    mensagem = " \n-Keller, Mary Kenneth (1965). Inductive inference on computer generated patterns (PhD Thesis). Madison, Wisconsin: The University of Wisconsin. (Doctoral Dissertation)\n-Computer graphics and applications of matrix methods : three dimensional computer graphics and projections by Mary K Keller; Consortium for Mathematics and Its Applications (U.S.); Undergraduate Mathematics and Its Applications Project (U.S.) Lexington, MA : COMAP/UMAP, 1983. U106, U110.\n-Electrical circuits and Applications of matrix methods : analysis of linear circuits Mary K Keller; Consortium for Mathematics and Its Applications (U.S.); Undergraduate Mathematics and Its Applications Project (U.S.), 1978. U108.\n-Food service management and Applications of matrix methods : food service and dietary requirements by Mary K Keller; Consortium for Mathematics and Its Applications (U.S.); Undergraduate Mathematics and Its Applications Project (U.S.) Lexington, MA : COMAP/UMAP, 1983. U105, U109.\n-Markov chains and applications of matrix methods : fixed point and absorbing Markov chains by Mary K Keller; Consortium for Mathematics and Its Applications (U.S.); Undergraduate Mathematics and Its Applications Project (U.S.) Lexington, MA : COMAP/UMAP, 1983. U107, U111."
     return mensagem
 
 
 def citacoes():
-    mensagem = ""
+    mensagem = "Pela primeira vez podemos simular mecanicamente o processo cognitivo. Podemos completar estudos em inteligência artificial. Além disso, este mecanismo [o computador] pode ser usado para auxiliar as pessoas no aprendizado. Visto que com o tempo teremos alunos mais maduros e em qualidade superior, este tipo de ensinamento será provavelmente sempre mais importante."
     return mensagem
 
 
@@ -33,7 +33,7 @@ def erro():
     return mensagem
 
 
-print("\nBoa noite! Você está aprendendo sobre Allan Turing.\n")
+print("\nBoa noite! Você está aprendendo sobre Mary Kenneth Keller.\n")
 
 continuar = True
 while continuar == True:
